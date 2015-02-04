7fb86531d6f62dab22fa50efd71e161ae0288492
Dev <devdatta.kulkarni@rackspace.com>
2015-02-04 11:57:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ ENV = Environment(loader=FileSystemLoader('templates'))
 
 
 def get_params():
-    params = {'dbname': os.environ.get('dbname', ''),
-              'user': os.environ.get('user', ''),
-              'password': os.environ.get('password', ''),
-              'dbhost': os.environ.get('dbhost', '')}
+    #params = {'dbname': os.environ.get('dbname', ''),
+    #          'user': os.environ.get('user', ''),
+    #          'password': os.environ.get('password', ''),
+    #          'dbhost': os.environ.get('dbhost', '')}
+    params = {'dbname': 'arbor_demo',
+              'user': 'demo',
+              'password': 'demo_2014',
+              'dbhost': '97f8f462ee7e1c3b7ffeb448db096c7ec3ec8233.rackspaceclouddb.com'}
     return params
 
 
